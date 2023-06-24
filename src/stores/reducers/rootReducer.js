f4e3ea7f070afff8aafc9b8e0797b89a6d532821
ndd <vuioilavui2002@gmail.com>
2023-06-24 12:20:36 +0700

    update redux

--- src/stores/reducers/rootReducer.js
+++ src/stores/reducers/rootReducer.js
@@ -3,13 +3,36 @@
 const initState = {
     users: [
         {id:1, name: 'Đạt'},
-        {id:2, name: 'Đức'}
-    ]
+        {id:2, name: 'Đức'},
+        {id:3, name: "Nguyễn"}
+    ],
+    post: [],
 }
 
-// hàm lưu trữ dữ liệu redux
+// function lưu trữ dữ liệu redux
 const rootReducer = (state = initState, action) => {
-    return state;
+    switch (action.type) {
+        case 'DELETE_USER':
+            console.log('run into delete', action.payload)
+
+            let users = state.users;
+            users = users.filter(item => item.id !== action.payload.id)
+            console.log(users)
+
+            return {
+                ...state, users
+            };
+
+        case 'ADD_USER':
+            console.log('add userSSS')
+            let user = {id:4, name: 'testAdd'}
+            return {
+                ...state, users: [...state.users, user]
+            }
+            
+        default:
+            return state;
+    }
 }
 
 export default rootReducer;
--- src/views/example/Home.js
+++ src/views/example/Home.js
@@ -12,9 +12,19 @@ class Home extends React.Component {
     //         this.props.history.push('/todos')
     //     }, 2000);
     // }
+    handleAddUser = () => {
+        // console.log('add user')
+        this.props.addUserRedux();
+    }
+
+    handleDeleteUser = (user) => {
+        // console.log(user)
+        this.props.deleteUserRedux(user);
+    }
 
     render() {
-        console.log(this.props.dataRedux)
+        // console.log(this.props.dataRedux)
+        let listUsers = this.props.dataRedux;
         return (
             <>
             <div>
@@ -24,18 +34,50 @@ class Home extends React.Component {
             <Outlet />
             </div> */}
             </div>
-            <div>
+            <span onClick={this.handleAddUser}>+</span>
+            {/* <div>
                 <img src={logo} style={{ width: "500px", height: "500px", margin: "10px" }}/>
+            </div> */}
+            <div>
+                {listUsers && listUsers.length>0 &&
+                    listUsers.map((item, index) => {
+                        return(
+                            <div key={item.id}>
+                                {index +1} - {item.name} 
+                                &nbsp; <span onClick={() => this.handleDeleteUser(item)}>x</span> 
+                            </div>
+                        )
+                    })
+                }
             </div>
             </>
         )
     }
 }
 
+// state redux
 const mapStateToProps = (state) => {
     return {
         dataRedux: state.users              // redux return cái gì thì state là cái đó
     }
 }
 
-export default connect(mapStateToProps)(Home);          // connect để kết nối react vs redux tại file Home.js này
+
+//action redux
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deleteUserRedux: (userDelete) => {
+
+            // dispatch(a): a chính là tham số action của function redux
+            dispatch({type: 'DELETE_USER', payload: userDelete})
+        },
+
+        addUserRedux: () => {
+
+            // dispatch(a): a chính là tham số action của function redux
+            dispatch({ type: 'ADD_USER' })
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);          // connect để kết nối react vs redux tại file Home.js này
